32cddfd904c8dd8e908d364bf6cdadcbffceede2
Noel Mugaviri <noel.mugaviri@f1006-05-34128.bmcc.cuny.edu>
2016-02-10 17:58:10 -0500

    initial commit

---
+++ week 2/app.js
@@ -0,0 +1,10 @@
+var submit = document.getElementById("submit");
+submit.onclick = function() {
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var country = document.getElementById("country").value;
+	var message = document.getElementById("message").value;
+	message.innerHTML = "Yes! " + 	
+
+	
+};
