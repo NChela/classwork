3d4125453b88cdd0d8fc2249c5bf67bcbbcf7115
Noel Mugaviri <noel.mugaviri@f1006-05-34128.bmcc.cuny.edu>
2016-02-17 19:31:06 -0500

    change

--- week 2/app.js
+++ week 2/app.js
@@ -1,10 +1,23 @@
 var submit = document.getElementById("submit");
 submit.onclick = function() {
-	var name = document.getElementById("name").value;
-	var email = document.getElementById("email").value;
-	var country = document.getElementById("country").value;
-	var message = document.getElementById("message").value;
-	message.innerHTML = "Yes! " + 	
+    
+var answer = document.getElementById("answer").value;
+console.log(answer);    
+if  (answer == 'The letter "e"'){
+   greeting = "Correct!"
+   
+   
+
+} else {
+    
+  greeting = "Nope. Sorry"
+}
+    
+    
+    
+	
+var message = document.getElementById("message");
+message.innerHTML = greeting; 	
 
 	
 };
