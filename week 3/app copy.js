2b33554837e24989f0723f8ce13a129c8e0537dd
Noel Mugaviri <noel.mugaviri@f1006-05-34128.bmcc.cuny.edu>
2016-02-24 19:08:51 -0500

    changes

---
+++ week 3/app copy.js
@@ -0,0 +1,31 @@
+var submit = document.getElementById("submit");
+submit.onclick = function() {
+    
+var response = [];    
+    
+    
+var responseElements = document.getElementsByClassName("response");
+console.log(response); 
+    
+
+    
+
+
+  if  (response == 'The letter "e"'){
+   greeting = "Correct!"
+   
+   
+
+} else {
+    
+  greeting = "Nope. Sorry"
+}
+    
+    
+    
+	
+var message = document.getElementById("message");
+message.innerHTML = greeting; 	
+
+	
+};
