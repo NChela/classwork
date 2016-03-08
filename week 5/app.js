60cf869f5a7a76b723cb464e12510d2f4d321a2a
Noel Mugaviri <noel.mugaviri@f1006-05-34128.bmcc.cuny.edu>
2016-03-07 19:15:18 -0500

    made changes to week index.html and app.js

--- week 5/app.js
+++ week 5/app.js
@@ -1,48 +1,76 @@
-var submit = document.getElementById("submit");
-submit.onclick = function() {
+var president = document.getElementById("president");
+president.onclick = function() {
     
 var answer = document.getElementById("answer").value;
 console.log(answer);    
-if  (answer == 'The letter "e"'){
-   greeting = "Correct!"
+if  (answer == 'George Washington'){
+   greeting = "YES! You are correct!!"
    
    
 
 } else {
     
-  greeting = "Nope. Sorry"
-}
-    
-var answer2 = document.getElementById("answer2").value;
-console.log(answer2);    
-if  (answer == 'The letter "e"'){
-   greeting = "Correct!"
-   
-   
-
-} else {
-    
-  greeting = "Nope. Sorry"
+  greeting = "Nope. Sorry try again."
 }
     
 
-var answer3 = document.getElementById("answer3").value;
-console.log(answer3);    
-if  (answer == 'The letter "e"'){
-   greeting = "Correct!"
-   
-   
-
-} else {
-    
-  greeting = "Nope. Sorry"
-}
-    
-    
     
 	
-var message = document.getElementById("message");
-message.innerHTML = greeting; 	
+var message1 = document.getElementById("message1");
+message1.innerHTML = greeting; 
+    
+};    
+    
+
+
+var country = document.getElementById("country");
+country.onclick = function() {
+    
+var land = document.getElementById("land").value;
+console.log(land);    
+if  (land == 'Africa'){
+   greeting = "CORRECT! Good job."
+   
+   
+
+} else {
+    
+  greeting = "Incorrect. Try again."
+}
+    
+
+    
+	
+var message2 = document.getElementById("message2");
+message2.innerHTML = greeting; 	
+    
+
+	
+};
+
+
+
+var ball = document.getElementById("ball");
+ball.onclick = function() {
+    
+var ring = document.getElementById("ring").value;
+console.log(ring);    
+if  (ring == '6'){
+   greeting = "You're right!!"
+   
+   
+
+} else {
+    
+  greeting = "Wrong. Try again."
+}
+    
+
+    
+	
+var message3 = document.getElementById("message3");
+message3.innerHTML = greeting; 	
+    
 
 	
 };
