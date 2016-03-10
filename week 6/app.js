f81fb00b009cc6b1502a50b212b0111903240175
Noel Mugaviri <noel.mugaviri@f1006-05-34128.bmcc.cuny.edu>
2016-03-09 19:15:05 -0500

    made changes to week 6

--- week 6/app.js
+++ week 6/app.js
@@ -1,76 +1,16 @@
-var president = document.getElementById("president");
-president.onclick = function() {
-    
-var answer = document.getElementById("answer").value;
-console.log(answer);    
-if  (answer == 'George Washington'){
-   greeting = "YES! You are correct!!"
-   
-   
+var submit = document.getElementById("answerButton");
+submit.onclick = function() {
 
-} else {
-    
-  greeting = "Nope. Sorry try again."
-}
-    
+var messages = [
+  'Maybe.', 'Certainly not.', 'I hope so.', 'Not in your wildest dreams.',
+  'There is a good chance.', 'Quite likely.', 'I think so.', 'I hope not.',
+  'I hope so.', 'Never!', 'Fuhgeddaboudit.', 'Ahaha! Really?!?', 'Pfft.',
+  'Sorry, bucko.', 'Hell, yes.', 'Hell to the no.', 'The future is bleak.',
+  'The future is uncertain.', 'I would rather not say.', 'Who cares?',
+  'Possibly.', 'Never, ever, ever.', 'There is a small chance.', 'Yes!'];
 
-    
-	
-var message1 = document.getElementById("message1");
-message1.innerHTML = greeting; 
-    
-};    
-    
-
-
-var country = document.getElementById("country");
-country.onclick = function() {
-    
-var land = document.getElementById("land").value;
-console.log(land);    
-if  (land == 'Africa'){
-   greeting = "CORRECT! Good job."
-   
-   
-
-} else {
-    
-  greeting = "Incorrect. Try again."
-}
-    
-
-    
-	
-var message2 = document.getElementById("message2");
-message2.innerHTML = greeting; 	
-    
-
-	
+document.getElementById('answerButton').onclick = function () 
+{var answer = messages[Math.floor(Math.random() * messages.length)];
+document.getElementById('message').innerHTML = answer;
 };
-
-
-
-var ball = document.getElementById("ball");
-ball.onclick = function() {
     
-var ring = document.getElementById("ring").value;
-console.log(ring);    
-if  (ring == '6'){
-   greeting = "You're right!!"
-   
-   
-
-} else {
-    
-  greeting = "Wrong. Try again."
-}
-    
-
-    
-	
-var message3 = document.getElementById("message3");
-message3.innerHTML = greeting; 	
-    
-
-	
-};
