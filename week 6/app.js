fb39f209b86407daf6694a96d427236e1195d99c
Noel Mugaviri <noel.mugaviri@f1006-05-34128.bmcc.cuny.edu>
2016-03-14 20:18:19 -0400

    changes to week 6

--- week 6/app.js
+++ week 6/app.js
@@ -1,16 +1,12 @@
-var submit = document.getElementById("answerButton");
-submit.onclick = function() {
-
-var messages = [
-  'Maybe.', 'Certainly not.', 'I hope so.', 'Not in your wildest dreams.',
-  'There is a good chance.', 'Quite likely.', 'I think so.', 'I hope not.',
-  'I hope so.', 'Never!', 'Fuhgeddaboudit.', 'Ahaha! Really?!?', 'Pfft.',
-  'Sorry, bucko.', 'Hell, yes.', 'Hell to the no.', 'The future is bleak.',
-  'The future is uncertain.', 'I would rather not say.', 'Who cares?',
-  'Possibly.', 'Never, ever, ever.', 'There is a small chance.', 'Yes!'];
-
-document.getElementById('answerButton').onclick = function () 
-{var answer = messages[Math.floor(Math.random() * messages.length)];
-document.getElementById('message').innerHTML = answer;
-};
+document.getElementById('answerClick').addEventListener('click', function(){
     
+console.log(this);
+    
+var x =  document.getElementById('list').value;
+ 
+document.body.appendChild(x); 
+message.innerHTML = 
+                                                        
+console.log(x);
+        
+});
