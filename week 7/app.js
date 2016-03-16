0686293d88bcdd1f2261476dcd44757c80b332a6
Noel Mugaviri <noel.mugaviri@f1006-05-34128.bmcc.cuny.edu>
2016-03-16 17:33:54 -0400

    added week 7

---
+++ week 7/app.js
@@ -0,0 +1,12 @@
+document.getElementById('answerClick').addEventListener('click', function(){
+    
+console.log(this);
+    
+var x =  document.getElementById('list').value;
+ 
+document.body.appendChild(x); 
+message.innerHTML = 
+                                                        
+console.log(x);
+        
+});
