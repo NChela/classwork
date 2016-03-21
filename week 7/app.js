896759e9ae6de9b4101f3ed1a22c9a8cd6913519
Noel Mugaviri <noel.mugaviri@f1006-05-34128.bmcc.cuny.edu>
2016-03-21 19:33:12 -0400

    added changes to week 7

--- week 7/app.js
+++ week 7/app.js
@@ -1,32 +1,19 @@
-var questions = ["Is the sky blue?", "What os the capital of Latvia?", "What is 2 + 2?"];
-                 
-var answers = [ "Yes", "Riga", "4", "Hello"];
-
-var makeQuestion = function(question, answer){
-
-var q = document.createElement("p")
-    q.innerHTML = "Is the sky blue?";
-    document.body.appendChild(q);
-
-var t = document.createElement("t")
-    t.type = "text";
-    t.placeholder = "Answer";
-    document.body.appendChild(t);
-
-var b = document.createElement("b")
-    b.innerHTML = "Submit Answer.?";
-    b.onclick = function() {
-
-var m = document.createElement("p")
-var a = t.value;
-
-if (a =="Yes") {
-    m.innerHTML = "Correct!";
-
-} else {
-    m.innerHTML = "Nope!";
+var submit = document.getElementById("submit");
+submit.addEventListener('click', function(event) {
     
+
+    var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
+    var pass = "";
+    for (var i = 0; i < 100; i += 10) {
+        var i = Math.floor(Math.random() * chars.length);
+        pass += chars.charAt(i);
+    }
     
+    var message = document.getElementById("message");
+    message.innerHTML = pass;
+});
+
+
 
                                   
 
