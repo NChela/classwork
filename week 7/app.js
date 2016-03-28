987a9fa5daa7908f98a223a22ffb39e5b281167b
Noel Mugaviri <noel.mugaviri@f1006-05-34128.bmcc.cuny.edu>
2016-03-28 17:39:01 -0400

    changes to week 7

--- week 7/app.js
+++ week 7/app.js
@@ -4,7 +4,7 @@ submit.addEventListener('click', function(event) {
 
     var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
     var pass = "";
-    for (var i = 0; i < 100; i += 10) {
+    for (var i = 0; i < 8; i++) {
         var i = Math.floor(Math.random() * chars.length);
         pass += chars.charAt(i);
     }
