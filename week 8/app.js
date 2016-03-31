73949a70487bb30fbd47f87cc3eda3c9ace31f73
Noel Mugaviri <noel.mugaviri@f1006-05-34128.bmcc.cuny.edu>
2016-03-30 20:40:14 -0400

    changes to week 8

--- week 8/app.js
+++ week 8/app.js
@@ -1,23 +1,21 @@
-var userInput = [];
+
 var answers = ["B", "D", "A", "C", "C", "D", "A", "C", "A", "B"]; 
 
+tot = answers.length;
+
+function getCheckedValue( radioName ){
+    var radios = document.getElementsByName( radioName ); // Get radio group by-name
+    for(var y=0; y<radios.length; y++)
+      if(radios[y].checked) return radios[y].value; // return the checked value
+}
+
 function getScore(){
-var score=0;
-var numQuestions=10;
-
-for (var i = 0; i < numQuestions; i++){
-console.log(document.getElementsByName("question" + i));    
-
-if (userInput[i] == answers[i]){
-score += 1;
-}
-else{
-score += 0;
+  var score = 0;
+  for (var i=0; i<tot; i++)
+    if(getCheckedValue("question"+i)===answers[i]) score += 1; // increment only
+  return score;
 }
 
-}
-return score;
-}
 function returnScore(){
-alert("Your score is "+getScore()+"/10");
+  alert("Your score is "+ getScore() +"/"+ tot);
 }
