34e42bae14b12924db5181f478cd7d4e3af22e0e
Noel Mugaviri <noel.mugaviri@f1006-05-34128.bmcc.cuny.edu>
2016-05-23 17:37:39 -0400

    final project

--- week 12/game.js
+++ week 12/game.js
@@ -1,42 +1,58 @@
 var player;
 
-var speed = 2;
+var speed = 0.1;
 
-var gravity = 2;
+var jump = 20;
+
+var jumping = false;
+
+
+var gravity = 1;
+
 
 var platform;
-
 var obstacle;
 
 
 function setup() {
 	createCanvas(720,360);
-    player = createSprite(100, 100, 40, 40);
-    platform = createSprite(width/2, height, width, 20);
-    obstacle = createSprite(300, height, 60, 40);
+    player = createSprite(400, 100, 40, 40);
+    platform = createSprite(width/2, height, width, 40);
+    obstacle = createSprite(300, height, 50, 100);
  
 }
 
 function draw() {
 	background("black");
-    console.log(player);
     
-    if ( keyWentDown("a") ) {
+    if ( keyDown("a") ) {
 	player.velocity.x -= speed;
     
-    } else if ( keyWentDown("d") ) {
+    } else if ( keyDown("d") ) {
 	player.velocity.x += speed;
     
     }
     
-    player.velocity.y += gravity
+    
+    
+    player.velocity.y += gravity;
     if ( player.collide(platform) ) {
 	     player.velocity.y = 0;
+        if (jumping) jumping = false;
     }
     
+    if ( keyDown( "space" ) && !jumping ) {
+		player.velocity.y -= jump;
+		jumping = true;
+	}
+    
     if ( player.collide(obstacle) ) {
-	player.remove();
-}
+        player.position.x = 400;
+		player.position.y = 0;
+		player.velocity.x = 0;
+	
+    
+    }
     
     drawSprites();
 }
